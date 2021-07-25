be96eeb84729c16b5aacffef89f3e38bec03642e
Savva Kodeikin <savva.kodeikin@gmail.com>
2021-07-25 20:02:59 +0200

    Initial commit, created layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Content = styled.div`
+  width: 70%;
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+const MainContent = styled(Content)`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Header = styled.nav`
+  background: #3a3a55;
+  color: white;
+  font-size: 1.5em;
+  width: 100%;
+  height: 5vh;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Main = styled.main`
+  padding: 1rem;
+  background: #1f2128;
+  width: 100%;
+  color: white;
+  flex: 3;
+`;
+const NavBar = styled.div`
+  padding: 1rem;
+  background: #9aaab7;
+  width: 100%;
+  flex: 1;
+`;
+const Footer = styled.footer`
+  background: #ff9637;
+  width: 100%;
+  height: 5vh;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>
+        <Content>Corners</Content>
+      </Header>
+      <MainContent>
+        <NavBar>
+          Navigation
+        </NavBar>
+        <Main>Main</Main>
+      </MainContent>
+      <Footer>
+        <Content>Footer</Content>
+      </Footer>
+    </Container>
   );
 }
 
