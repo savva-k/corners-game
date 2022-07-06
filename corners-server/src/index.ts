0c64d4ec19b5c0d77420d71414f0646f5636d4ac
Savva Kodeikin <savva.kodeikin@gmail.com>
2022-07-06 18:04:57 +0200

    Added express

--- corners-server/src/index.ts
+++ corners-server/src/index.ts
@@ -1,9 +1,22 @@
 import WebsocketServer from "./websocket/WebsocketServer";
+import express from "express";
 
+const wsPort = 8080;
+const httpPort = 8090;
 process.title = "cornersServer";
 
-const wsServer = WebsocketServer(8080);
+const wsServer = WebsocketServer(wsPort);
 wsServer.start();
+
+const app = express();
+
+app.get("/login", (reg, res) => {
+  res.json("There will be some nice login feature one day");
+});
+
+app.listen(httpPort, () => {
+  console.log(`REST service is listening on ${httpPort}`)
+})
 
 process.on("SIGINT", function () {
   console.log("\nGracefully shutting down from SIGINT");
