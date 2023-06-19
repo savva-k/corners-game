a4e74ee2a28f87f0c39f6bb5c13ea9ed287de8b4
Savva Kodeikin <savva.kodeikin@gmail.com>
2023-06-19 20:54:48 +0200

    Removed custom login logic

--- corners-server/src/index.ts
+++ corners-server/src/index.ts
@@ -4,8 +4,6 @@ import bodyParser from "body-parser";
 import Knex from "knex";
 import knexConfig from "./db/knexfile";
 import * as dotenv from "dotenv";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
 
 dotenv.config();
 
@@ -24,54 +22,8 @@ app.use(
   })
 );
 
-const knex = Knex(knexConfig.development);
-knex.on("query", console.log);
-
-const getUserByLogin = (login: string) => {
-  return knex("users")
-    .select("id", "name", "password")
-    .where("name", login)
-    .first();
-};
-
-app.post("/login", (req, res) => {
-  const { login, password } = req.body;
-
-  return getUserByLogin(login).then((user) => {
-    if (user) {
-      return bcrypt.compare(password, user.password).then((success) => {
-        if (success) {
-          return res.json({
-            token: jwt.sign({ user: user.name }, process.env.TOKEN_SECRET),
-          });
-        }
-        return res.sendStatus(401);
-      });
-    }
-    return res.sendStatus(401);
-  });
-});
-
-app.post("/register", (req, res) => {
-  const { login, email, password } = req.body;
-
-  return getUserByLogin(login).then((user) => {
-    if (!user) {
-      return bcrypt.hash(password, 10).then((hash) => {
-        return knex("users")
-          .insert({ name: login, email: email, password: hash })
-          .then(() => {
-            return res.sendStatus(200);
-          })
-          .catch((err) => {
-            console.error(err);
-            return res.sendStatus(500);
-          });
-      });
-    }
-    return res.sendStatus(400);
-  });
-});
+// const knex = Knex(knexConfig.development);
+// knex.on("query", console.log);
 
 app.listen(httpPort, () => {
   console.log(`REST service is listening on ${httpPort}`);
