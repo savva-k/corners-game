bcedff56f6945f6e6a700391b93427b47c918a0d
Savva Kodeikin <savva.kodeikin@gmail.com>
2024-01-13 18:15:05 +0100

    Added a test for a full game with a draw result (both players reach home)

--- backend/ws-server/src/test/java/com/playcorners/service/CornersGameServiceTest.java
+++ backend/ws-server/src/test/java/com/playcorners/service/CornersGameServiceTest.java
@@ -25,7 +25,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 @QuarkusTest
 public class CornersGameServiceTest {
@@ -53,6 +52,18 @@ public class CornersGameServiceTest {
             "g5", "a5", "a6", "e6", "d6", "a6", "a7", "g5", "e3", "d3", "d4", "d5", "b3", "b4", "b5", "d6", "b4", "f3",
             "d5", "f5", "d3", "d5", "d6", "c3", "c2", "e4", "e5", "e3", "c3", "e5", "e6", "b2", "a2", "f1", "f5", "b4",
             "b2");
+
+    private static final List<String> fullGameDrawBothHome = List.of("c2", "c4",
+            "f7", "f5", "b2", "d4", "g7", "e5", "d4", "d5", "h7", "h5", "c3", "e3", "f8", "f7", "c1", "c2", "f7", "d7",
+            "c2", "e2", "h8", "b2", "d3", "f3", "e5", "c3", "d2", "f4", "f6", "d4", "a1", "e1", "h6", "d6", "e2", "g4",
+            "e7", "c5", "f4", "h6", "g8", "f8", "e3", "g7", "d6", "f2", "b1", "c1", "f8", "d6", "f3", "g3", "d4", "d3",
+            "d1", "h3", "d6", "d2", "b3", "b4", "e8", "d8", "c4", "d4", "d8", "f4", "a2", "e4", "d7", "e7", "g7", "g8",
+            "e7", "e3", "g3", "g7", "d3", "b1", "c1", "d1", "f5", "b3", "a3", "a4", "g6", "g5", "d1", "f5", "e3", "d3",
+            "e4", "e5", "g5", "g3", "e1", "e2", "e6", "a2", "a4", "e6", "h5", "h4", "h3", "h7", "h4", "h3", "b4", "c4",
+            "c5", "b5", "e6", "e7", "h3", "f1", "c4", "e8", "f1", "e1", "g4", "g5", "b5", "a5", "e2", "g6", "g3", "f3",
+            "d4", "e4", "a5", "a4", "e8", "f8", "f3", "d1", "g8", "h8", "f4", "f3", "e4", "g8", "e1", "a1", "f5", "f6",
+            "f2", "e2", "d5", "f7", "f3", "e3", "e7", "e8", "e3", "c1", "e5", "e6", "a4", "b4", "g5", "f5", "b3", "a3",
+            "f5", "e5", "e2", "c2", "e5", "e7", "b4", "b3");
 
     @BeforeEach
     public void setup() {
@@ -149,12 +160,11 @@ public class CornersGameServiceTest {
 
     @Test
     public void givenWhiteHasOneMoveToWin_whenWhiteMakesWinningMove_thenBlackStillHaveOneMoveToMakeDraw() {
-        fail();
-    }
-
-    @Test
-    public void givenBlackHasOneMoveToWin_whenBlackMakesWinningMove_thenBlackWins() {
-        fail();
+        Game game = startGame();
+        assertDoesNotThrow(() -> makeMoves(game, fullGameDrawBothHome));
+        assertTrue(game.isFinished());
+        assertNull(game.getWinner());
+        assertEquals(FinishReason.DrawBothHome, game.getFinishReason());
     }
 
     @Test
