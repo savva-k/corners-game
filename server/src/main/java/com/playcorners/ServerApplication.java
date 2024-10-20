2fe243cd22455e631f5b3c85ad3a481da9da6f19
Savva Kodeikin <savva.kodeikin@gmail.com>
2024-10-20 16:23:29 +0200

    Remove test log statement

--- server/src/main/java/com/playcorners/ServerApplication.java
+++ server/src/main/java/com/playcorners/ServerApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServerApplication {
 
   public static void main(String[] args) {
-    System.out.println("Test");
-    SpringApplication.run(ServerApplication.class, args);
+      SpringApplication.run(ServerApplication.class, args);
   }
 
 }
