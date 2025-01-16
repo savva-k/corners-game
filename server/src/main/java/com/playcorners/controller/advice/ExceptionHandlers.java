18a7086f0d68a1414d4eb574c01779cbae7512ed
Savva Kodeikin <savva.kodeikin@gmail.com>
2025-01-16 16:30:35 +0100

    Intercept CommonGameException and transform into json response

--- server/src/main/java/com/playcorners/controller/advice/ExceptionHandlers.java
+++ server/src/main/java/com/playcorners/controller/advice/ExceptionHandlers.java
@@ -1,6 +1,7 @@
 package com.playcorners.controller.advice;
 
 import com.playcorners.controller.message.ErrorResponse;
+import com.playcorners.service.exception.CommonGameException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,5 +17,10 @@ public class ExceptionHandlers {
             code = e.getFieldError().getDefaultMessage();
         }
         return ResponseEntity.badRequest().body(new ErrorResponse(code));
+    }
+
+    @ExceptionHandler(value = CommonGameException.class, produces = "application/json")
+    public ResponseEntity<ErrorResponse> handle(CommonGameException e) {
+        return ResponseEntity.badRequest().body(new ErrorResponse(e.getReason().toString()));
     }
 }
