693badd418ed87775cbd9bbe993f393341f58ff1
Savva Kodeikin <savva.kodeikin@gmail.com>
2025-03-10 23:07:04 +0100

    Fix gaps between map tiles by switching to CANVAS
    Looks like a tech debt!

--- frontend/src/phaser/main.ts
+++ frontend/src/phaser/main.ts
@@ -1,4 +1,4 @@
-import { AUTO, Game, Types } from 'phaser';
+import { CANVAS, Game, Types } from 'phaser';
 import { GAME_CONTAINER_ID, GLOBAL_REGISTRY_GAME_DATA, GLOBAL_REGISTRY_PLAYER, GLOBAL_REGISTRY_TRANSLATIONS } from './constan';
 import { Game as MainGame } from './scenes/Game';
 import { Game as GameModel } from '../model/Game';
@@ -7,13 +7,14 @@ import { Player } from '../model';
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Types.Core.GameConfig = {
-    type: AUTO,
+    type: CANVAS, //todo: AUTO is resolved to WEBGL which causes some gaps between tiles on mobiles sometimes
     width: '100%',
     height: '80%',
     parent: GAME_CONTAINER_ID,
     pixelArt: true,
+    antialias: false,
     scale: {
-        mode: Phaser.Scale.FIT,
+        mode: Phaser.Scale.NONE, // FIT to enable scaling when type: AUTO issue is fixed. Gaps are present even when mode = NONE and type = AUTO
         autoCenter: Phaser.Scale.CENTER_BOTH,
     },
     scene: [
