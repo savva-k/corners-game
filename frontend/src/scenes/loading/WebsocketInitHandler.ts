10e39db0a37fc5df2e7e8ad8406da310afd54d36
Savva Kodeikin <savva.kodeikin@gmail.com>
2025-09-23 21:21:10 +0200

    Don't pass player through all the scenes

--- frontend/src/scenes/loading/WebsocketInitHandler.ts
+++ frontend/src/scenes/loading/WebsocketInitHandler.ts
@@ -2,7 +2,6 @@ import type { WebSocketConnection } from "../../WebSocket";
 import { EventBus } from "../../EventBus";
 import { type Game as GameModel } from '../../model/Game';
 import type { WebsocketInit } from "./WebsocketInitScene";
-import { getPlayerFromJwt } from "../../utils/JwtUtil";
 
 export class WebsocketInitHandler {
     ws: WebSocketConnection;
@@ -19,7 +18,7 @@ export class WebsocketInitHandler {
 
     public activate() {
         EventBus.once('CREATE_OR_LOAD_GAME_OK', (gameData: GameModel) => {
-            this.wsInitScene.continueLoading(gameData, getPlayerFromJwt());
+            this.wsInitScene.continueLoading(gameData);
         });
     }
 }
