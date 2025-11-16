cf8f466cf92fe79afd1a662ad9080886884b371d
Savva Kodeikin <savva.kodeikin@gmail.com>
2025-11-16 20:29:58 +0100

    Increase log level in Vite dev config

--- frontend/vite/config.dev.mjs
+++ frontend/vite/config.dev.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
     base: './',
+    logLevel: 'warning',
     server: {
         port: 3000,
         allowedHosts: ['.localhost', 'test-fe.playcorners.ru']
